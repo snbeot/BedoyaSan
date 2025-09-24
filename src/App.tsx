3f403983993509eeb041d933804a75535ef111ca
San <snbeot25@gmail.com>
2025-09-23 20:28:59 -0500

    Dev commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
       <div>
         <p>Hello world!</p>
         <p>My name's San, this is my test website</p>
+        <hr />
+        <p>This is the development branch</p>
       </div>
     </>
   )
